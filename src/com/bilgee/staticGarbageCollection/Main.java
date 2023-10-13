ceac1c177bc20e8764a350c3e7057be870822be4
Bilge927 <bilegdemberel.bayasgalanbayar@stud.sti.se>
2023-10-13 11:02:35 +0200

    2

--- src/com/bilgee/staticGarbageCollection/Main.java
+++ src/com/bilgee/staticGarbageCollection/Main.java
@@ -1,7 +1,33 @@
 package com.bilgee.staticGarbageCollection;
 
 public class Main {
+
+
+// DEPRECATED
+   /* public void finalize () {
+        System.out.println("Test Garbage Collection");
+        }
+    */
+
     public static void main(String[] args) {
+
+        // Counter counterA = new Counter();
+        // Counter counterB = new Counter();
+        Counter counterC = new Counter();
+        Counter counterD = new Counter();
+
+
+
+
+        // Garbage Collection - Mark for unreferenced Objects (heap)
+        new Counter(); // Anonymous object
+        counterC = null;  // Nulling Reference
+        counterC = counterD; // Assigning Reference ( to another )
+
+        System.out.println(counterC); // Same Memory Address
+        System.out.println(counterD); // Same Memory Address
+
+        System.gc();
 
 
 
